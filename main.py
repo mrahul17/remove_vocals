dad58e6c599398e889f2de23f1e982950a5d8c4f
light94 <mishra.rahul1712@gmail.com>
2016-02-22 22:23:42 +0530

    Add function to remove vocals
    Current algo based on
    http://www.danielzingaro.com/sound_proc2/assignment.html

--- main.py
+++ main.py
@@ -4,14 +4,34 @@ import sys
 import sound
 
 
-def open_file(file_name):
+def open_file(file_path):
     ''' return a sound object for the given file name'''
-    return sound.load_sound(file_name)
+    return sound.load_sound(file_path)
 
+def remove_vocals(sound_obj):
+    num_samples = len(sound_obj)
+    new_sound = sound.create_sound(num_samples)
+
+    for i in range(num_samples):
+        orig_samp = sound.get_sample(sound_obj,i)
+        new_samp = sound.get_sample(new_sound,i)
+        left_ch = sound.get_left(orig_samp)
+        right_ch = sound.get_right(orig_samp)
+        result = int( (left_ch - right_ch) / 2.0 )
+        sound.set_left(new_samp,result)
+        sound.set_right(new_samp,result)
+
+    return new_sound
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
-    sound_obj = open_file(file_name)
+    file_path = sys.argv[1]
+    file_name = file_path.split('/')[-1]
+    file_name_split = file_name.split('.')
+    sound_obj = open_file(file_path)
+    new_sound = remove_vocals(sound_obj)
+    new_file_name = file_name_split[0] + '_novoice' + '.wav' 
+    new_sound.save_as(new_file_name)
 
+    print "Success.. Exiting.."
 
 
